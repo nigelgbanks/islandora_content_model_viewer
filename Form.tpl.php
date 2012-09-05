619958f5ae56515def0cd2208de4ca73896e0b23
Nigel Banks <nigel.g.banks@gmail.com>
2012-09-05 12:38:09 -0400

    Debug messages now printing.

--- Form.tpl.php
+++ Form.tpl.php
@@ -1,13 +1,16 @@
 <div id="<?php print $wrapper_id; ?>" class="drupal-content">
+   <?php $form = ''; ?>
    <?php switch($action):
 case 'edit': ?>
-   <?php print drupal_get_form('content_model_viewer_edit_metadata_form', $pid); ?>
+   <?php $form = drupal_get_form('content_model_viewer_edit_metadata_form', $pid); ?>
    <?php break;?>
    <?php case 'ingest-concept': ?>
-   <?php print drupal_get_form('content_model_viewer_ingest_concept_metadata_form', $pid); ?>
+   <?php $form = drupal_get_form('content_model_viewer_ingest_concept_metadata_form', $pid); ?>
    <?php break;?>
    <?php case 'ingest-resource': ?>
-   <?php print drupal_get_form('content_model_viewer_ingest_resource_metadata_form', $pid); ?>
+   <?php $form =  drupal_get_form('content_model_viewer_ingest_resource_metadata_form', $pid); ?>
    <?php break;?>
    <?php endswitch; ?>
+   <?php print theme_status_messages(); ?>
+   <?php print $form; ?>
 </div>
